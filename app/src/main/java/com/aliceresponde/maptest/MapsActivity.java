ed1017aa073b654a478a2463ca072fb340e6ed4a
Alicia Beltran <aliceresponde@gmai.com>
2015-09-06 22:07:03 -0500

    listView fixed

--- app/src/main/java/com/aliceresponde/maptest/MapsActivity.java
+++ app/src/main/java/com/aliceresponde/maptest/MapsActivity.java
@@ -1,30 +1,26 @@
 package com.aliceresponde.maptest;
 
+import android.app.ActionBar.LayoutParams;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.Toast;
-
-import android.app.ActionBar.LayoutParams;
-
 
 import com.github.rahatarmanahmed.cpv.CircularProgressView;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -46,11 +42,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private EditText et_address_pop;
     CircularProgressView progressView ;
 
-    private LatLng latLng;
-    private MarkerOptions markerOptions;
+    private  LatLng latLng;
+    private  LatLng selected_latLng;
+    private  MarkerOptions markerOptions;
 
     private ListView lv_addresses ;
-    private List<String> list_resut_address;
+    private List<String> list_resut_address= new ArrayList<String>();
     private List<Address> list_rest_from_gc;
     private String str_user="";
 
@@ -98,6 +95,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
+
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
@@ -118,17 +116,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         popupWindow.setTouchable(true);
         popupWindow.setFocusable(true);
 
-        popupWindow.getBackground().setAlpha(0);
-//        popupWindow.setModal(true);
-
         // Removes default black background
-//        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         //Dismiss Listener
         popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
-                Toast.makeText(getApplicationContext(), "out", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "out"+ str_user + selected_latLng, Toast.LENGTH_LONG).show();
 
             }
         });
@@ -139,9 +134,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         et_address_pop = (EditText) popupView.findViewById(R.id.et_address_pop);
         et_address_pop.setText(str_user);
         et_address_pop.addTextChangedListener(new TextWatcher() {
-
             public void afterTextChanged(Editable s) {
-
                 Toast.makeText(getApplicationContext(), "editing Text", Toast.LENGTH_LONG).show();
             }
 
@@ -160,6 +153,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     new ArrayList<String>()
                 ));
 
+        //Accion para buscar  una vez se ha seleccionado una opcion
+        lv_addresses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                str_user= list_resut_address.get(position);
+                Address aux_address= list_rest_from_gc.get(position);
+                selected_latLng = new LatLng(aux_address.getLatitude(), aux_address.getLongitude());
+                popupWindow.dismiss();
+
+            }
+        });
+
         Log.i("user1", str_user);
 
         if(str_user.length()!=0) {
@@ -173,7 +178,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     // An AsyncTask class for accessing the GeoCoding Web Service
 
 
-    private class GeocoderTask extends AsyncTask<String, Void, Void> implements com.aliceresponde.maptest.GeocoderTask {
+    private class GeocoderTask extends AsyncTask<String, Void, Void> {
         private ArrayAdapter<String> adapter;
         private Geocoder gc ;
 
@@ -211,10 +216,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             else {
                 Log.i("found", list_rest_from_gc.size() + "");
                 for (Address i : list_rest_from_gc) {
+//                    Log.i("addresL[0]", i.getAddressLine(0));
+//                    Log.i("addresL[1]", i.getAddressLine(1));
+//                    Log.i("addresL[2]", i.getAddressLine(2));
                     String addressText = String.format("%s, %s",
                             i.getMaxAddressLineIndex() > 0 ? i.getAddressLine(0) : "",
-                            i.getCountryName());
-                    Log.i(""+i, addressText);
+                            i.getAddressLine(1).split(",")[0]);
+                    Log.i("" + i, addressText);
+                    String s  ="";
+                    s=i.getAddressLine(1).length()>0? i.getAddressLine(1):"";
+                    list_resut_address.add(s);
+
                     adapter.add(addressText);
                     lv_addresses.setAdapter(adapter);
                 }
